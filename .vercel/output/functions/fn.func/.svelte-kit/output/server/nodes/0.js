379efd898a071d388c0008028aaa3ae5c81914ea
lauhard.andreas <lauhard.andreas@gmail.com>
2023-06-12 09:44:10 +0200

    test5

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,7 +0,0 @@
-
-
-export const index = 0;
-export const component = async () => (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.578c7f2c.js","_app/immutable/chunks/index.fa6fede3.js"];
-export const stylesheets = [];
-export const fonts = [];
